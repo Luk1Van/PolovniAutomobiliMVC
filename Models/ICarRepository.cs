de7d4951a7fc7dbef26ab29993991cb0d861699e
иван лукич <ivkurcic@gmail.com>
2023-04-28 04:17:58 +0300

    Add project files.

---
+++ Models/ICarRepository.cs
@@ -0,0 +1,9 @@
+﻿namespace PolovniAutomobiliMVC.Models
+{
+    public interface ICarRepository
+    {
+        IEnumerable<Car> AllCars { get; }
+        IEnumerable<Car> SpecialOfferCars { get; }
+        Car GetCarById(int id);
+    }
+}
---
+++ Models/ICategoryRepository.cs
@@ -0,0 +1,8 @@
+﻿namespace PolovniAutomobiliMVC.Models
+{
+    public interface ICategoryRepository
+    {
+        IEnumerable<Category> AllCategories { get; }
+
+    }
+}
